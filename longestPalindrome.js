3c51133b2cd2c19b6f4cd7a96f1ed8f7ca068261
ljw <leijiawang@analysys.com.cn>
2017-10-20 18:01:39 +0800

    longestPalindrome

--- longestPalindrome.js
+++ longestPalindrome.js
@@ -2,15 +2,25 @@
  * @param {string} s
  * @return {string}
  */
+//c b b d
 var longestPalindrome = function(s) {
     let longest = ''
-    for(let i=0;i<s.length;i++){
+    for(let i=0;i<=s.length-1;i+=0.5){
         const boundary = Math.min(i,s.length-i-1);
-        for(let j=0;j<boundary;j++){
-
+        let sub = i%1?'':s.charAt(i)
+        for(let j=1;j<=boundary+0.5;j++){
+            const prev = s.charAt(Math.ceil(i-j))
+            const next = s.charAt(Math.floor(i+j))
+            if(prev===next){
+                sub = prev+sub+next
+            }else {
+                break
+            }
+        }
+        if(sub.length>longest.length){
+            longest = sub
         }
     }
-    console.log(longest)
     return longest
 };
 var isPalindrome = function (s) {
@@ -23,4 +33,4 @@ var isPalindrome = function (s) {
     }
     return true
 }
-longestPalindrome('abcba')
+longestPalindrome("babad")
